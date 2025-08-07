d10bdb485499fecebddebbf39df0d4ee490ec6e5
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-08-07 23:08:58 +0530

    Add selection sort implementation in JavaScript
    Introduces a function to perform selection sort on an array. The function iterates through the array, selecting the minimum element and swapping it into place, returning the sorted array.

---
+++ Selection_sort.js
@@ -0,0 +1,16 @@
+selectionSort(arr) {
+        // your code here
+        let n=arr.length;
+        for(let i=0;i<n;i++){
+            let Min=i;
+            for(let j=i+1;j<n;j++){
+                if(arr[j]<arr[Min]){
+                    Min=j;
+                }
+            }
+            let temp=arr[Min];
+            arr[Min]=arr[i];
+            arr[i]=temp;
+        }
+        return arr;
+    }
