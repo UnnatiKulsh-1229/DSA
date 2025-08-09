093ff90501067a5b0f3f4c76f3038f34b11dda04
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-08-09 20:17:48 +0530

    Add insertion sort implementation in JavaScript

---
+++ Insertion_sort.js
@@ -0,0 +1,15 @@
+class Solution {
+    insertionSort(arr) {
+        // code here
+        for(let i=1;i<arr.length;i++){
+            let key=arr[i];
+            let j=i-1;
+            while(arr[j]>key && j>=0){
+                arr[j+1]=arr[j];
+                j=j-1;
+            }
+            arr[j+1]=key;
+        }
+        return arr;
+    }
+}
