25c74fde06cacb3346c5d39b554e7ab5f75c5c3c
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-08-25 23:06:19 +0530

    Next permutation

---
+++ LC_PROB_31.js
@@ -0,0 +1,27 @@
+var nextPermutation = function(nums) {
+    let n = nums.length;
+    let ind = -1;
+    for (let i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            ind = i;
+            break;
+        }
+    }
+    if (ind === -1) {
+        nums.reverse();
+        return;
+    }
+    for (let i = n - 1; i > ind; i--) {
+        if (nums[i] > nums[ind]) {
+            [nums[i], nums[ind]] = [nums[ind], nums[i]];
+            break;
+        }
+    }
+    let left = ind + 1;
+    let right = n - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+};
