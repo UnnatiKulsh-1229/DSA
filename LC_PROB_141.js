3461c22dab7693cddfdb5bc3b52f14910c6acbca
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-09-11 20:18:47 +0530

    Add solution for Linked List Cycle (LeetCode 141)
    Implements Floyd's Tortoise and Hare algorithm to detect cycles in a linked list. Returns true if a cycle is found, otherwise false.

---
+++ LC_PROB_141.js
@@ -0,0 +1,11 @@
+//Floyd's Algo
+var hasCycle = function(head) {
+    let slow=head;
+    let fast=head;
+    while((slow && fast && fast.next)!==null){
+        slow=slow.next;
+        fast=fast.next.next;
+        if(slow===fast) return true;
+        };
+        return false;
+}
