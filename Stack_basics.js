8e52817aca206cdce2d6e063504bed2ffc68d1b7
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-10-20 00:06:19 +0530

    Update Stack_basics.js

--- Stack_basics.js
+++ Stack_basics.js
@@ -38,3 +38,31 @@ class myStack {
         return this.arr[this.top];
     }
 }
+
+//Stack using Queue
+
+var MyStack = function() {
+    this.q1=[];
+};
+MyStack.prototype.push = function(x) {
+    this.q1.push(x);
+    let n=this.q1.length;
+    for(let i=0;i<n-1;i++){
+        this.q1.push(this.q1[0]);
+        this.q1.shift();
+    }
+};
+MyStack.prototype.pop = function() {
+    if (this.q1.length=== 0) return -1;
+    return this.q1.shift();
+};
+MyStack.prototype.top = function() {
+    if (this.q1.length === 0)
+            return -1;
+        return this.q1[0];
+};
+
+MyStack.prototype.empty = function() {
+    if(this.q1.length===0) return true;
+    return false;
+};
