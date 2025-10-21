b14862a25a55059824735654de9396b849de166c
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-10-22 00:32:03 +0530

    Update queue_basics.js

--- queue_basics.js
+++ queue_basics.js
@@ -46,3 +46,58 @@ class myQueue {
         return this.arr[this.size-1];
     }
 }
+
+//Queue using Stack(LC_PROB_232)
+var MyQueue = function() {
+    this.s=[];    
+};
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.s.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if(this.s.length===0) return -1;
+    let x=this.s.pop();
+    if(this.s.length===0) return x;
+    let item = this.pop();
+    this.s.push(x); 
+    return item;
+};
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+        if(this.s.length===0) return -1;
+        let x = this.s.pop();
+
+        if (this.s.length === 0) {
+            this.s.push(x);
+            return x;
+        }
+           // recursive call
+        let item = this.peek();
+        this.s.push(x);
+        return item;
+    }
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.s.length===0;
+};
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
