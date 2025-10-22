e6000c93df809c583471d2c1a6ea999512f307c2
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-10-22 20:42:41 +0530

    Update Stack_basics.js

--- Stack_basics.js
+++ Stack_basics.js
@@ -66,3 +66,57 @@ MyStack.prototype.empty = function() {
     if(this.q1.length===0) return true;
     return false;
 };
+
+//Stack using Linked list(GFG)
+// Node class
+/* class Node {
+    constructor(new_data) {
+        this.data = new_data;
+        this.next = null;
+    }
+} */
+
+class myStack {
+    constructor() {
+        this.top=null;
+        this.len=0
+        // Initialize your data members
+    }
+
+    isEmpty() {
+        // check if the stack is empty
+        return this.top===null;
+    }
+
+    push(x) {
+        // Adds an element x at the top of the stack
+        let temp=new Node(x);
+        temp.next=this.top;
+        this.top=temp;
+        this.len++;
+    }
+
+    pop() {
+        // Removes an element from the top of the stack
+        if(this.top===null) return -1;
+        let temp=this.top;
+        this.top=this.top.next;
+        let v=temp.data;
+        temp=null;
+        this.len--;
+        return v;
+    }
+
+    peek() {
+        // Returns the top element of the stack
+        // If stack is empty, return -1
+        if(this.top===null) return -1;
+        return this.top.data;
+    }
+
+    size() {
+        // Returns the current size of the stack
+        return this.len;
+        
+    }
+}
