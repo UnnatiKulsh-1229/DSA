3ea418e0a0e914aa303cc3e4a63315670aee66ba
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-10-23 22:59:57 +0530

    Add solution for valid parentheses problem
    Implements  isValid function to check if a string of parentheses is valid by using a stack.

---
+++ LC_PROB_20.js
@@ -0,0 +1,22 @@
+{/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack=[];
+    for(let i=0;i<s.length;i++){
+        if(s[i]==="("|| s[i]==="{"|| s[i]==="["){
+            stack.push(s[i]);
+        }
+        else if(s[i]===")"|| s[i]==="}"|| s[i]==="]"){
+            if(stack.length===0) return false;
+            let top = stack[stack.length - 1];
+            if((s[i]===")" && top!=="(")||(s[i]==="}" && top!=="{")||(s[i]==="]" && top!=="[")){
+                return false;
+            }
+            stack.pop();
+        }
+        }
+        return stack.length===0;
+}
+};
