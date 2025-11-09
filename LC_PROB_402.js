bc2133f90f618e225198f49e2ebd63939b67920d
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-11-09 20:15:08 +0530

    Create LC_PROB_402.js

---
+++ LC_PROB_402.js
@@ -0,0 +1,27 @@
+var removeKdigits = function(num, k) {
+    let stack=[];
+    for(let i=0;i<num.length;i++){
+        while (stack.length > 0 && k > 0 && stack[stack.length - 1] > num[i]) {
+    stack.pop();
+    k--;
+}
+
+        stack.push(num[i]);
+    }
+    while(k>0){
+        stack.pop();
+        k--;
+    }
+    if(stack.length===0) return "0";
+    let res="";
+    while(stack.length>0){
+        res+=stack[stack.length-1];
+        stack.pop();
+    }
+    while(res.length!==0 && res[res.length-1]==='0'){
+        res = res.slice(0, -1);
+        }
+    res=res.split("").reverse().join("");
+    return res === "" ? "0" : res;
+
+};
