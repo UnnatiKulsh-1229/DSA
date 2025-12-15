12824be593d2da4555493327bd9e255dbe4568ec
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-12-16 00:18:49 +0530

    Add solution for LeetCode problem 424

---
+++ LC_PROB_424.js
@@ -0,0 +1,16 @@
+var characterReplacement = function(s, k) {
+    let l=0;
+    let maxlen=0;
+    let res={};
+    let maxfreq=0;
+    for (let r = 0; r < s.length; r++) {
+        res[s[r]] = (res[s[r]] || 0) + 1;
+        maxfreq = Math.max(maxfreq, res[s[r]]);
+        while ((r - l + 1) - maxfreq > k) {
+            res[s[l]]--;
+            l++;
+        }
+        maxlen=Math.max(maxlen,r-l+1);
+    }
+    return maxlen;
+};
