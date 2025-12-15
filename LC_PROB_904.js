e40ff9a67815d197870784c60d58e522e3e1d8ea
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-12-15 23:47:26 +0530

    Update LC_PROB_904.js

--- LC_PROB_904.js
+++ LC_PROB_904.js
@@ -14,3 +14,25 @@ var totalFruit = function(fruits) {
     }
     return maxlen;
 };
+
+// Optimal sliding window
+
+var totalFruit = function(fruits) {
+    let l=0;
+    let r=0;
+    let res=new Map();
+    let maxlen=0;
+    while(r<fruits.length){
+        res.set(fruits[r], (res.get(fruits[r]) || 0) + 1);
+        while(res.size > 2){
+                res.set(fruits[l], res.get(fruits[l]) - 1);
+                if (res.get(fruits[l]) === 0) {
+                res.delete(fruits[l]);
+            }
+                l++;
+        }
+        maxlen=Math.max(maxlen,r-l+1);
+        r++;
+    }
+    return maxlen;
+};
