b3f9fe66784850487f34c6f98533d75ffaf28a10
Unnati Kulshrestha <unkl.dolly@gmail.com>
2025-12-18 20:10:43 +0530

    Add optimal solution for longest substring with k unique chars
    Introduces a new implementation using a sliding window and a Map to efficiently find the longest substring with exactly k unique characters. The new solution improves performance over previous approaches.

--- longest_substr_w_k_uniquechar.js
+++ longest_substr_w_k_uniquechar.js
@@ -17,3 +17,28 @@ class Solution {
         return maxlen;
     }
 }
+
+//optimal solution
+class Solution {
+    longestKSubstr(s, k) {
+        // code here
+        let l=0;
+        let r=0;
+        let maxlen=-1;
+        let res=new Map();
+        while(r<s.length){
+             res.set(s[r],(res.get(s[r])||0)+1);
+             while(res.size>k){
+                 res.set(s[l], res.get(s[l]) - 1);
+                if (res.get(s[l]) === 0) res.delete(s[l]);
+                l++;
+
+             }
+             if(res.size===k){
+                 maxlen=Math.max(maxlen,r-l+1)
+             }
+            r++;
+        }
+    return maxlen;
+    }
+}
