0596de2cd9b47dfdb8a986561301e765fd769b54
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-01-22 23:56:21 +0530

    Add solution for LeetCode problem 347 (Top K Frequent Elements)

---
+++ LC_PROB_347.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<int[]> p=new PriorityQueue<>((a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            p.add(new int[]{entry.getValue(),entry.getKey()});
+            if(p.size()>k) p.poll();
+        }
+        int[] res=new int[k];
+        int idx=0;
+        while(!p.isEmpty()){
+            res[idx++]=p.poll()[1];
+        }
+        return res;
+
+    }
+}
