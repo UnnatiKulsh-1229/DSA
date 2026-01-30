52533d13674273dd6750016e461350b2d14b29dc
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-01-30 20:02:07 +0530

    Create LC_PROB_435.java

---
+++ LC_PROB_435.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        int cnt=0;
+        int prevend=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<prevend){
+                cnt++;
+            }
+            else{
+                prevend=intervals[i][1];
+            }
+        }
+        return cnt;
+    }
+}
