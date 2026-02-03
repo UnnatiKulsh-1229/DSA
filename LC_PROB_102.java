e02c99a26c88adf4e1d0229bfa81c36def151488
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-02-03 14:05:16 +0530

    Create LC_PROB_102.java

---
+++ LC_PROB_102.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n=q.size();
+            List<Integer> lvl=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                TreeNode node=q.poll();
+                lvl.add(node.val);
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
+            }
+            res.add(lvl);
+        }
+        return res;
+    }
+}
