23cc2c9ce6dd6ec8e20142e7e8059ba9d595026a
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-02-06 21:44:28 +0530

    Create LC_PROB_145(2stack).java

---
+++ LC_PROB_145(2stack).java
@@ -0,0 +1,20 @@
+ //using 2 stacks
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        if(root==null) return res; 
+        Stack<TreeNode> s1=new Stack<>();
+        Stack<TreeNode> s2=new Stack<>();
+        s1.push(root);
+        while(!s1.isEmpty()){
+            root=s1.pop();
+            s2.push(root);
+            if(root.left!=null) s1.push(root.left);
+            if(root.right!=null) s1.push(root.right);
+        }
+        while(!s2.isEmpty()){
+            res.add(s2.pop().val);
+        }
+        return res;
+    }
+}
