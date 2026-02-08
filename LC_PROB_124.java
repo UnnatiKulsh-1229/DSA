45227f73904d9089d8946a2bb330c1d67e31cfb6
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-02-09 00:02:24 +0530

    Create LC_PROB_124.java

---
+++ LC_PROB_124.java
@@ -0,0 +1,14 @@
+class Solution {
+    int res=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    public int dfs(TreeNode root){
+        if(root==null) return 0;
+        int l=Math.max(0,dfs(root.left));
+        int r=Math.max(0,dfs(root.right));
+        res=Math.max(res,l+r+root.val);
+        return Math.max(l,r)+root.val;
+    }
+}
