7ef7878a6c209b48aaea12f78fd0eb7e69fb9c9a
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-02-18 20:30:16 +0530

    Update LC_PROB_98.java

--- LC_PROB_98.java
+++ LC_PROB_98.java
@@ -1,3 +1,4 @@
+// Checks for valid bst using helper function that checks if the value of the current node is within the range of min and max values allowed for that node. The range is updated as we traverse down the tree.
 class Solution {
     public boolean isValidBST(TreeNode root) {
         return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
